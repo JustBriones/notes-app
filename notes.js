988e29c50edcebdde9e7102dfc8be27f9ba64106
Justin <justbriones@gmail.com>
2017-06-01 21:35:03 -0700

    added debugging and list function

--- notes.js
+++ notes.js
@@ -34,7 +34,7 @@ var addNote = (title, body) => {
 
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -52,6 +52,7 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+  debugger;
   console.log('--');
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+  name: "justin"
+};
+
+person.age = 25;
+
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
